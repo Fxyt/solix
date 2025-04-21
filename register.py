2415813a84ead7c0d81bd2bc8fbd7fecef32bfbb
Fxyt <59430774+Fxyt@users.noreply.github.com>
2025-04-22 00:54:43 +0700

    Update register.py

--- register.py
+++ register.py
@@ -6,9 +6,12 @@ def regist(email,password,refferal_code,i):
     url = 'https://api.solixdepin.net/api/auth/register'
     payload = {"email":email,"password":password,"captchaToken":solver(),"referralCode":refferal_code}
     response = requests.post(url,headers=headers('post'),json=payload).json()
-    if response['result'] == 'success':
-        print(f'[{i}] email {email} successful register')
-        save_account(email,password)
+    try:
+        if response['result'] == 'success':
+            print(f'[{i}] email {email} successful register')
+            save_account(email,password)
+    except Exception as e:
+        print(e)
 
 def run():
     refferal_code = input('input your refferal code: ')
